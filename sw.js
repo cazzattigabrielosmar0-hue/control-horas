df381f163044bfa8f7bc7e0dc25fe5d876516221
cazzattigabrielosmar0-hue <cazzattigabrielosmar0@gmail.com>
2026-06-25 13:59:06 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,18 +7,6 @@ self.addEventListener("install", event => {
   );
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(nombres =>
-      Promise.all(
-        nombres
-          .filter(nombre => nombre !== CACHE_NAME)
-          .map(nombre => caches.delete(nombre))
-      )
-    )
-  );
-});
-
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(respuesta => respuesta || fetch(event.request))
