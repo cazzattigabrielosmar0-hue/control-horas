92b10022f20ad06d80d99b134973322b5c34ae53
cazzattigabrielosmar0-hue <cazzattigabrielosmar0@gmail.com>
2026-06-25 23:11:01 -0300

    Upgrade service worker to control-horas-v6
    Updated service worker to version 6 with cache management and activation handling.

--- sw.js
+++ sw.js
@@ -1,9 +1,23 @@
-const CACHE_NAME = "control-horas-v5";
+const CACHE_NAME = "control-horas-v6";
 const ARCHIVOS = ["index.html", "style.css", "app.js", "manifest.json"];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ARCHIVOS))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ARCHIVOS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(nombres =>
+      Promise.all(
+        nombres
+          .filter(nombre => nombre !== CACHE_NAME)
+          .map(nombre => caches.delete(nombre))
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
