6c9ba4fbe4dc8eeb96d4f793f94b194e03d87835
cazzattigabrielosmar0-hue <cazzattigabrielosmar0@gmail.com>
2026-06-29 07:33:26 -0300

    Update service worker to version 8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "control-horas-v7";
+const CACHE_NAME = "control-horas-v8";
 const ARCHIVOS = ["index.html", "style.css", "app.js", "manifest.json"];
 
 self.addEventListener("install", event => {
