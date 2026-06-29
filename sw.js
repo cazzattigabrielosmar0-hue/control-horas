274b391f9825111868b23ffd2ddb67c254b0a24d
cazzattigabrielosmar0-hue <cazzattigabrielosmar0@gmail.com>
2026-06-29 09:52:59 -0300

    Update service worker cache version to v9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "control-horas-v8";
+const CACHE_NAME = "control-horas-v9";
 const ARCHIVOS = ["index.html", "style.css", "app.js", "manifest.json"];
 
 self.addEventListener("install", event => {
